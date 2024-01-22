9e027318f32be78cd4378bf38ef89d0762dbe2ef
teacherbot <norrisa@faytechcc.edu>
2024-01-22 16:17:01 +0000

    completed M1T2

---
+++ M1T2_Norris.cpp
@@ -0,0 +1,24 @@
+// CSC 134
+// M1T2
+// Name
+// 1/22/24
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Introduce yourself
+    cout << "Hello World!" << endl;
+    cout << "My name is Andrew Norris" << "\n";
+    cout << "I teach C++ and machine learning" << endl;
+    
+    // Ask a question
+    string name;
+    cout << "What's your name? ";
+    cin >> name;
+
+    cout << "Nice to meet you, " << name << endl;
+
+  cout << endl << endl;
+  return 0;
+}
