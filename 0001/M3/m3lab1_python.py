945461655ececb277df5931bf314dea6351d82ea
teacherbot <norrisa@faytechcc.edu>
2024-03-01 01:05:20 +0000

    data class example py

--- 0001/M3/m3lab1_python.py
+++ 0001/M3/m3lab1_python.py
@@ -10,11 +10,22 @@ class InventoryItem:
     def total_cost(self) -> float:
         return self.unit_price * self.quantity_on_hand
 
+    def __str__(self) -> str:
+        return self.name + "/" + str(self.unit_price) + "/" + str(self.quantity_on_hand)
 
 def main():
     bread = InventoryItem("bread", 2.99, 1)
     print(bread)
     print("$",bread.total_cost())
+
+    store_bag = []
+    for item in ["bread", "sword", "backpack"]:
+        my_item = InventoryItem(item, 1.99, 1)
+        store_bag.append(my_item)
+
+    #print(store_bag)
+    for thing in store_bag:
+        print("You have:", thing)
 
 
 main()
